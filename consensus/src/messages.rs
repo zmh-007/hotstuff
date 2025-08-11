90ec60744b022ad2db139c409e8ccdb0f4e7e050
zmh <zmh@zmhdeMacBook-Pro.local>
2025-08-11 17:23:05 +0800

    update block hash

--- consensus/src/messages.rs
+++ consensus/src/messages.rs
@@ -116,15 +116,17 @@ impl Block {
 
 impl Hash for Block {
     fn digest(&self) -> Digest {
-        let tx_tail = self.tx_tail();
+        let tx_tail = self.payload.iter().map(|v| {Fr::deserialize_be_compressed(&v.0[..]).expect("Failed to deserialize tx hash to Fr")}).hash();
+        let txg: Tx = self.txg[..].try_into().expect("Failed to convert txg bytes to Tx");
         let elements = vec![
             self.author.to_hash(),
             self.round.to_field(),
             self.qc.hash.to_field(),
             self.qc.last_tail.to_field(),
+            txg.hash(),
             Fr::deserialize_be_compressed(&self.next.0[..]).expect("Failed to deserialize next1 to Fr"),
             Fr::deserialize_be_compressed(&self.next.1[..]).expect("Failed to deserialize next2 to Fr"),
-            Fr::deserialize_be_compressed(&tx_tail.0[..]).expect("Failed to deserialize tx_tail to Fr"),
+            tx_tail,
         ];
 
         let mut b = Vec::new();
